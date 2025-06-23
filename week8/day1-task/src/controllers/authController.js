e64644c0756bb522b3e6a6975a109cfc0e411af7
birajgrs <birajghorasaine11@gmail.com>
2025-06-23 17:50:30 +0545

    all parts completed (2 varification in progress for security )

--- week8/day1-task/src/controllers/authController.js
+++ week8/day1-task/src/controllers/authController.js
@@ -1,14 +1,14 @@
-import bcrypt from 'bcrypt';
-import User from '../models/User.js';
-import LoginLog from '../models/LoginLog.js';
-
+import bcrypt from "bcrypt";
+import User from "../models/User.js";
+import LoginLog from "../models/LoginLog.js";
+// import { sendPhoneOTP, verifyPhoneOTP } from "./otpController.js";
 
 export const register = async (req, res) => {
   try {
     const { name, email, password, role, department } = req.body;
 
     const existing = await User.findOne({ email });
-    if (existing) return res.send('Email already registered');
+    if (existing) return res.send("Email already registered");
 
     const hashed = await bcrypt.hash(password, 10);
 
@@ -17,41 +17,40 @@ export const register = async (req, res) => {
       email,
       password: hashed,
       role,
-      department
+      department,
     });
 
-    res.redirect('/login');
+    res.redirect("/login");
   } catch (err) {
-    res.status(500).send('Registration failed');
+    res.status(500).send("Registration failed");
   }
 };
-
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (!user || !(await bcrypt.compare(password, user.password))) {
-    return res.send('Invalid credentials');
+    return res.send("Invalid credentials");
   }
 
   req.session.user = {
     _id: user._id,
     name: user.name,
     role: user.role,
-    department: user.department
+    department: user.department,
   };
 
   await LoginLog.create({
     userId: user._id,
     ip: req.ip,
-    userAgent: req.headers['user-agent']
+    userAgent: req.headers["user-agent"],
   });
 
-  res.redirect('/dashboard');
+  res.redirect("/dashboard");
 };
 
 export const logout = (req, res) => {
   req.session.destroy();
-  res.redirect('/login');
+  res.redirect("/login");
 };
